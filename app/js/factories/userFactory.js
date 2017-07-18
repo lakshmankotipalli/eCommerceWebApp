9c1d6354103d074fd90f821a5c36faa4fe74c892
Lakshman <lakshman.kotipalli@outlook.com>
2017-07-18 23:21:01 +0530

    * Minor changes

---
+++ app/js/factories/userFactory.js
@@ -0,0 +1,13 @@
+ecomm.factory('userFactory', ['HttpService', '$interpolate', function (HttpService, $interpolate) {
+
+  var data = {
+    login: 'data/data.json'
+  };
+
+  var factory = {};
+
+  factory.userAuth = function (obj) {
+
+  };
+
+}]);
